0c0d27d019ddc4713f373399fbebba1c4fac49b9
alupusmc <48524504+alupusmc@users.noreply.github.com>
2019-09-28 18:57:48 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopComponent } from './top/top.component';
+import { MidComponent } from './mid/mid.component';
+import { BotComponent } from './bot/bot.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, TopComponent ],
+  declarations: [ AppComponent, TopComponent, MidComponent, BotComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
